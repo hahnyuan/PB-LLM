bdeab08261917ee50c3fa1b4a8e5ce64f9548e32
42Shawn <36666427+42Shawn@users.noreply.github.com>
2023-07-04 09:56:49 -0500

    save model

--- bnn_train_layerwise_w_KD.py
+++ bnn_train_layerwise_w_KD.py
@@ -165,6 +165,7 @@ def main(args):
 
         # Train the model
         trainer.train()
+    trainer.save_model(output_dir=args.model_save_dir)
 
 
 if __name__ == "__main__":
@@ -183,6 +184,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--debug", action="store_true", help="Debug mode (only 10 steps)"
+    )
+    parser.add_argument(
+        "--model_save_dir", type=str, default="./checkpoints/vanilla_kd", help="saving model to this directory"
     )
     args = parser.parse_args()
 
