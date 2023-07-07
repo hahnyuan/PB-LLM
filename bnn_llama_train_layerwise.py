49a02997cf3d98d83a69a8e968058942b60255d0
42Shawn <36666427+42Shawn@users.noreply.github.com>
2023-07-07 13:19:36 -0500

    Update bnn_llama_train_layerwise.py
    successfully ran

--- bnn_llama_train_layerwise.py
+++ bnn_llama_train_layerwise.py
@@ -23,7 +23,7 @@ from utils import *
 
 def main(args):
     tokenizer = AutoTokenizer.from_pretrained(args.model_id)
-    model = AutoModelForCausalLM.from_pretrained(args.model_id, device_map={"": 0})
+    model = AutoModelForCausalLM.from_pretrained(args.model_id, device_map='auto')
 
     # Enable gradient checkpointing and prepare model for k-bit training
     model.gradient_checkpointing_enable()
@@ -34,7 +34,7 @@ def main(args):
 
     generate_ids = model.generate(inputs.input_ids, max_length=50)
     outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-    print(outputs)
+    # print(outputs)
 
     # Load dataset
     data = load_dataset(args.dataset)
@@ -43,7 +43,7 @@ def main(args):
     # Slice first 1000 samples
     # data = data['train'].select(range(1500))
     print('prepare training data')
-    data = data.map(lambda samples: tokenizer(samples["text"], truncation=True, max_length=128), batched=True, batch_size=1000, num_proc=os.cpu_count())
+    data = data.map(lambda samples: tokenizer(samples["text"], truncation=True, max_length=512), batched=True, batch_size=100, writer_batch_size=5000, num_proc=os.cpu_count())
     for name, _ in model.named_modules():
         print(name)
     layers = [(f"layer{i}", _) for i, _ in enumerate(model.base_model.layers)]
@@ -83,12 +83,12 @@ def main(args):
             per_device_train_batch_size=1,
             gradient_accumulation_steps=4,
             warmup_steps=100,
-            max_steps=10 if args.debug else 1000,
+            max_steps=10 if args.debug else 200,
             learning_rate=1e-4,
             fp16=True,
             logging_steps=1,
             output_dir="outputs",
-            optim="adamw_torch",
+            optim="adamw_hf",
             report_to="tensorboard",
             logging_dir='./outputs/runs/logging/',
         )
@@ -114,7 +114,11 @@ def main(args):
 
         # Train the model
         trainer.train()
-    trainer.save_model(output_dir='./checkpoints/lamma7b-ste')
+    generate_ids = model.generate(inputs.input_ids, max_length=50)
+    binarized_output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    print(binarized_output)
+    print('saving binarized model')
+    trainer.save_model(output_dir='./checkpoints/lamma7b-xnor')
 
 
 
@@ -127,7 +131,7 @@ if __name__ == "__main__":
         "--dataset", type=str, default='togethercomputer/RedPajama-Data-1T-Sample', help="Dataset name"
     )
     parser.add_argument(
-        "--order", type=str, default="forward", choices=["forward", "reverse"]
+        "--order", type=str, default="reverse", choices=["forward", "reverse"]
     )
     parser.add_argument(
         "--binarization_method", type=str, default="ste", choices=["ste", "ir", "fda", 'xnor']
