d9dca36960349b885daafe88e416b475144ef87c
42Shawn <36666427+42Shawn@users.noreply.github.com>
2023-07-16 23:01:58 -0500

    Update quant.py

--- quant.py
+++ quant.py
@@ -77,22 +77,30 @@ class BinaryExceptOutliersLinear(nn.Module,BinaryInterface):
             self.bias = nn.Parameter(bias.to(torch.float32).data)
         else:
             self.bias = None
+        self.printed = False
     
     def binarize_except_outliers(self):
         w = self.weight
         w_flat = w.view(-1)
-
         # lower_threshold, upper_threshold = torch.quantile(w_flat, torch.tensor([0.01, 0.99]).to(w.device))
 
-        mean = torch.mean(w_flat)
-        std = torch.std(w_flat) 
-        lower_threshold = mean - 3*std
-        upper_threshold = mean + 3*std
+        mean = torch.mean(w_flat).to(w.device)
+        std = torch.std(w_flat).to(w.device)
+        lower_threshold = mean - 1*std
+        upper_threshold = mean + 1*std
 
         outliers = (w < lower_threshold) | (w > upper_threshold)
+        # if self.printed is not True:
+        #     print(outliers.sum()/outliers.numel())
+        #     self.printed = True
 
         w_bin = w.clone()
+        # scaling_factor = w[~outliers].abs().mean(-1).view(-1, 1).detach()
+
+        w = STEBinary().apply(w)
         w_bin[~outliers] = STEBinary().apply(w[~outliers])
+        # w_bin[~outliers] = w[~outliers] * scaling_factor
+
         
         return w_bin
 
@@ -100,6 +108,8 @@ class BinaryExceptOutliersLinear(nn.Module,BinaryInterface):
         # w = STEBinary().apply(self.weight)
         w = self.binarize_except_outliers()
         return F.linear(x, w, self.bias)
+
+class BinaryXnorExceptOutliersLinear(nn.Module,BinaryInterface):
 
 class XnorBinaryLinear(nn.Module,BinaryInterface):
     def __init__(self, weight, bias) -> None:
