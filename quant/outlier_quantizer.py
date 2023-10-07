70b9ddc0b93163fdd5ca015ce534a6b6f9a69400
42Shawn <36666427+42Shawn@users.noreply.github.com>
2023-10-07 12:49:15 -0500

    Update outlier_quantizer.py

--- quant/outlier_quantizer.py
+++ quant/outlier_quantizer.py
@@ -6,42 +6,6 @@ from torch.utils.checkpoint import checkpoint
 from .quantizer import STEBinary, IrNetBinary, FdaBinary, BinaryInterface
 
 
-# class BinaryExceptOutliersLinear(nn.Module, BinaryInterface):
-#     def __init__(self, weight, bias) -> None:
-#         super().__init__()
-#         self.weight = nn.Parameter(weight.to(torch.float32).data)
-#         if bias is not None:
-#             self.bias = nn.Parameter(bias.to(torch.float32).data)
-#         else:
-#             self.bias = None
-#         self.printed = False
-
-#     def binarize_except_outliers(self):
-#         w = self.weight
-#         w_flat = w.view(-1)
-#         # lower_threshold, upper_threshold = torch.quantile(w_flat, torch.tensor([0.01, 0.99]).to(w.device))
-#         mean = torch.mean(w_flat).to(w.device)
-#         std = torch.std(w_flat).to(w.device)
-#         lower_threshold = mean - 1.96 * std
-#         upper_threshold = mean + 1.96 * std
-#         outliers = (w < lower_threshold) | (w > upper_threshold)
-#         # if self.printed is not True:
-#         #     print(outliers.sum()/outliers.numel())
-#         #     self.printed = True
-#         w_bin = w.clone()
-#         # scaling_factor = w[~outliers].abs().mean(-1).view(-1, 1).detach()
-#         w = STEBinary().apply(w)
-#         w_bin[~outliers] = STEBinary().apply(w[~outliers])
-#         # w_bin[~outliers] = w[~outliers] * scaling_factor
-
-#         return w_bin
-
-#     def forward(self, x):
-#         # w = STEBinary().apply(self.weight)
-#         w = self.binarize_except_outliers()
-#         return F.linear(x, w, self.bias)
-
-
 class BinaryXnorExceptOutliersLinear(nn.Module, BinaryInterface):
     def __init__(self, weight, bias, outlier_scale=1) -> None:
         super().__init__()
@@ -63,14 +27,30 @@ class BinaryXnorExceptOutliersLinear(nn.Module, BinaryInterface):
 
             mean = torch.mean(w_flat).to(w.device)
             std = torch.std(w_flat).to(w.device)
-            lower_threshold = mean - 0.670 * std  # 1.65 : 90%, 0.67 : 50%, 1.0, 70%
-            upper_threshold = mean + 0.670 * std  # 1.96 : 95%, 2.32 : 98%, 
+            lower_threshold = mean - 1.6 * std  # 1.6 : 90%, 0.67 : 50%, 1.0, 70%
+            upper_threshold = mean + 1.6 * std  # 1.95 : 95%, 2.3 : 98%, 
 
             outliers = (w < lower_threshold) | (w > upper_threshold)
             print(
                 f"Generat outlier_mask, outlier_fraction: {outliers.sum()}/{outliers.numel()}({outliers.sum()/outliers.numel()})"
             )
             self.outlier_mask = outliers.detach()
+
+            # import matplotlib.pyplot as plt
+            # import numpy as np
+
+            # # Assuming you have run your provided code and obtained the outliers tensor
+            # # Convert the tensor to numpy for visualization
+            # outliers_np = outliers.cpu().numpy()
+            # outliers_np = outliers_np[0:255,0:255]
+
+            # plt.figure(figsize=(10, 10))
+            # plt.imshow(outliers_np, cmap='hot', interpolation='nearest')
+            # plt.colorbar()
+            # plt.title('Outliers Matrix Visualization')
+            # plt.savefig('./111.pdf')
+            # # plt.show()
+
             self.binary_scale = (
                 w[~self.outlier_mask].abs().mean(-1).view(-1, 1).detach()
             )
